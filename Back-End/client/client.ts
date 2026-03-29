376214f062064352d836c6b7597f5785cd29c6f5
Truong Nguyen <truong.nguyen201@gmail.com>
2026-03-29 16:33:53 -0400

    Update files

--- Back-End/client/client.ts
+++ Back-End/client/client.ts
@@ -2,7 +2,11 @@ import { createClient } from '@supabase/supabase-js';
 import "dotenv/config"
 
 const projectURL = process.env.SUPABASE_URL;
-const APIKey = process.env.SUPABASE_KEY
+const APIKey = process.env.SUPABASE_KEY;
+
+if(!projectURL || !APIKey){
+    throw new Error("Missing Supabase environment variables")
+}
 
 const supabase = createClient(projectURL, APIKey);
 
