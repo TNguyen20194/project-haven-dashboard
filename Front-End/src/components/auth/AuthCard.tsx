d62ae412e3697c6dd367201d39d3e5836cb8b16b
Truong Nguyen <truong.nguyen201@gmail.com>
2026-04-18 18:53:08 -0400

    Update AuthPage

--- Front-End/src/components/auth/AuthCard.tsx
+++ Front-End/src/components/auth/AuthCard.tsx
@@ -1,7 +1,28 @@
+import { z } from "zod";
+
+import { useEffect } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { useForm } from "@tanstack/react-form";
+
+import { Card, CardContent } from "../ui/card";
+import Button from "@/UI/button";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { Tabs, TabsList, TabsContent } from "../ui/tabs";
+
+import { createUser, loginUser } from "@/lib/auth-storage";
+import { type AuthMode, useAuthUiStore } from "@/stores/auth-ui.store";
+
+const passwordSchema = z
+  .string()
+  .min(8, "Password must be at least 8 characters.")
+  .regex(/[A-Z]/, "Please include at least 1 uppercase character.")
+  .regex(/[a-z]/, "Please include at least 1 lowercase character.")
+  .regex(/\d/, "Please include at least 1 number.")
+  .regex(/[^A-Za-z0-9]/, "Please include at least 1 special character.");
+
 const AuthCard = () => {
-    return (
-        <h3>TEST</h3>
-    )
+  return <h3>TEST</h3>;
 };
 
 export default AuthCard;
