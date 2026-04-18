09471bb79e07939389bbfbfe6dafbac6517d0ac7
Truong Nguyen <truong.nguyen201@gmail.com>
2026-04-18 19:18:55 -0400

    Update from schema

--- Front-End/src/components/auth/AuthCard.tsx
+++ Front-End/src/components/auth/AuthCard.tsx
@@ -15,11 +15,48 @@ import { type AuthMode, useAuthUiStore } from "@/stores/auth-ui.store";
 
 const passwordSchema = z
   .string()
-  .min(8, "Password must be at least 8 characters.")
-  .regex(/[A-Z]/, "Please include at least 1 uppercase character.")
-  .regex(/[a-z]/, "Please include at least 1 lowercase character.")
-  .regex(/\d/, "Please include at least 1 number.")
-  .regex(/[^A-Za-z0-9]/, "Please include at least 1 special character.");
+  .min(8, { message: "Password must be at least 8 characters long" })
+  .regex(/[A-Z]/, { message: "Must include at least one uppercase letter" })
+  .regex(/[a-z]/, { message: "Must include at least one lowercase letter" })
+  .regex(/\d/, { message: "Must include at least one number" })
+  .regex(/[^A-Za-z0-9]/, {
+    message: "Must include at least one special character",
+  });
+
+const signupSchema = z
+  .object({
+    fullName: z
+      .string()
+      .min(3, { message: "Full name must be at least 3 characters." }),
+    email: z.email({ message: "Please enter a valid email." }),
+    password: passwordSchema,
+    confirmPassword: z
+      .string()
+      .min(1, { message: "Please confirm your password." }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ["confirmPassword"], // Path of error
+    message: "Make sure password match.",
+  });
+
+const loginSchema = z.object({
+  email: z.email({ message: "Please enter a valid email." }),
+  password: z.string().min(1, { message: "Please enter your password" }),
+});
+
+type AuthFormValues = {
+  fullName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
+const defaultValues: AuthFormValues = {
+    fullName: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
+}
 
 const AuthCard = () => {
   return <h3>TEST</h3>;
